6cbf3ad045d0441782f33ce2043602f17b1c81c6
TyrusHWaterloo <t4ha@uwaterloo.ca>
2026-02-07 21:57:05 +0000

    save changes

--- w26_healthtech_team14/client/src/App.jsx
+++ w26_healthtech_team14/client/src/App.jsx
@@ -1,18 +1,50 @@
+import { useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 import { TodoListCard } from './components/TodoListCard';
 import { Greeting } from './components/Greeting';
+import { NewPage } from './components/NewPage';
 
 function App() {
+    const [currentPage, setCurrentPage] = useState('home');
+
     return (
         <Container>
-            <Row>
-                <Col md={{ offset: 3, span: 6 }}>
-                    <Greeting />
-                    <TodoListCard />
-                </Col>
-            </Row>
+            {currentPage === 'home' && (
+                <>
+                    <Row>
+                        <Col md={{ offset: 3, span: 6 }}>
+                            <Greeting />
+                            <TodoListCard />
+                            <Button 
+                                onClick={() => setCurrentPage('newpage')}
+                                className="mt-3"
+                            >
+                                Go to New Page
+                            </Button>
+                        </Col>
+                    </Row>
+                </>
+            )}
+
+            {currentPage === 'newpage' && (
+                <>
+                    <Row>
+                        <Col md={{ offset: 3, span: 6 }}>
+                            <NewPage />
+                            <Button 
+                                onClick={() => setCurrentPage('home')}
+                                variant="secondary"
+                                className="mt-3"
+                            >
+                                Back to Home
+                            </Button>
+                        </Col>
+                    </Row>
+                </>
+            )}
         </Container>
     );
 }
---
+++ w26_healthtech_team14/client/src/components/NewPage.jsx
@@ -0,0 +1,8 @@
+export function NewPage() {
+    return (
+        <div>
+            <h1>New Page</h1>
+            <p>This is your new page content. Add whatever you want here!</p>
+        </div>
+    );
+}
